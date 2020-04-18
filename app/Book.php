556be4c4570c571d94cc23bf32fe93a293aa6a22
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2020-04-18 15:58:23 +0100

    Revert "Merge branch 'book-management' into audit-logs"
    This reverts commit d55ede23e60cc7ca52977ae53d7ca470b28b8e6e.

--- app/Book.php
+++ app/Book.php
@@ -4,26 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
-use Storage;
 
 class Book extends Model
 {
     use softDeletes;
     //
     protected $fillable = ['title', 'author', 'path', 'front_cover'];
-
-    public function book_count()
-    {
+    
+    public function book_count() {
         return $this->all()->count();
-    }
-
-    public function get_front_cover()
-    {
-        return Storage::url($this->front_cover);
-    }
-
-    public function get_download_link()
-    {
-        return Storage::url($this->path);
     }
 }
